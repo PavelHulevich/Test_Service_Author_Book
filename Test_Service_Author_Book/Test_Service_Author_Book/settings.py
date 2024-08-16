b9733b03b21ec15c4ca44d749715b5356f783faa
PavelHulevich <ferit.gomel@gmail.com>
2024-08-16 10:46:01 +0300

    change DB on sqlite3

--- Test_Service_Author_Book/Test_Service_Author_Book/settings.py
+++ Test_Service_Author_Book/Test_Service_Author_Book/settings.py
@@ -77,13 +77,16 @@ WSGI_APPLICATION = 'Test_Service_Author_Book.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'author_book',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'author_book',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'root',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
